653486d466a6d9305d737d0d14007927ba687a85
Thiago Souza <thiagofernandosouza20@gmail.com>
2023-03-06 12:03:18 -0300

    selecting elements

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const dice = document.querySelector('.dice')
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+dice.classList.add('hidden')
